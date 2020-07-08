c48b79127a391f1519ef04748ba422f929fd38ec
Ibrahim Ragab <ibrahim@wuilt.com>
2020-07-08 15:48:31 +0200

    Configure Outputs in a Custom Github Action

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const axios = require("axios");
+const core = require("@actions/core");
+
+const DEFAULT_CHARACTER = "dr-zoidberg";
 
 async function run() {
+  const character = core.getInput("character") || DEFAULT_CHARACTER;
+  console.log(character);
+
   const response = await axios.get(
-    "http://futuramaapi.herokuapp.com/api/characters/dr-zoidberg/1"
+    `http://futuramaapi.herokuapp.com/api/characters/${character}/1`
   );
   const { data } = response;
   const firstEntry = data[0];
-  console.log(`${firstEntry.charcter} ${firstEntry.quote}`);
-  // console.log("Hello, world!", response);
+  console.log(`${firstEntry.character} ${firstEntry.quote}`);
+
+  core.setOutput("quote", firstEntry);
 }
 
 run();
