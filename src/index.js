a255147eb52c0f8be1638015cafddda03005275f
Ibrahim Ragab <ibrahim@wuilt.com>
2020-07-08 18:05:14 +0200

    Debug Logs

--- src/index.js
+++ src/index.js
@@ -8,17 +8,27 @@ const availableCharacters = ["bender", "fry", "leela", "dr-zoidberg"];
 async function run() {
   const character = core.getInput("character") || DEFAULT_CHARACTER;
 
+  core.debug(`[futurama] input character: ${character}`);
+
   if (!availableCharacters.includes(character)) {
     core.setFailed(`Unkown character : ${character}`);
     return;
   }
 
+  core.debug(`[futurama] Retrieving quote for: ${character}`);
+
   const response = await axios.get(
     `http://futuramaapi.herokuapp.com/api/characters/${character}/1`
   );
+
+  core.debug(`[futurama] Successfully retrieved quote for: ${character}`);
+
   const { data } = response;
+
+  core.debug(`[futurama] Data: ${JSON.stringify(data, null, 2)}`);
+
   const firstEntry = data[0];
-  console.log(`${firstEntry.character} ${firstEntry.quote}`);
+  // console.log(`${firstEntry.character} ${firstEntry.quote}`);
 
   core.setOutput("quote", firstEntry);
 }
